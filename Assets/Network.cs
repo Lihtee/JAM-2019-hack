d5351c6d93f35477c97a3fa1bdbe8d6f2ad7747e
dDouble_V <double2vee@gmail.com>
2019-01-26 00:03:03 +0500

    new

---
+++ Assets/Network.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkNode
+{
+    public int id;
+    public List<int> connections;
+
+}
+
+
+public class Network : MonoBehaviour
+{
+    
+    public List<NetworkNode> nodes = new List<NetworkNode>(0);
+
+}
