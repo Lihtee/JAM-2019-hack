0fe08d00e7ebe8d2113ee233950c3c3da9e0d929
dDouble_V <double2vee@gmail.com>
2019-01-26 00:02:44 +0500

    comm_sceneloader
    qqqq

---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void onStartClick()
+    {
+        Debug.Log("Scene loading");
+    }
+
+    public void onExitClick()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
